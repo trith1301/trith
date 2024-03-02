7e1a844a3654c17670d69c02c4f7174f36eb7757
Tri Tran <trith13102@gmail.com>
2024-03-02 12:18:23 +0700

    :hammer: Update ENV

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const About = () => {
   return (
     <div>
-      <div className="container mt-6 mx-auto px-6"></div>
+      <div className="container mt-6 mx-auto px-6">
+        Current Environment: {process.env.CURRENT_ENV}
+      </div>
     </div>
   )
 }
